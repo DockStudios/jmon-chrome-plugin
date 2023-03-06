2a48939da0c126068c37d443aecce1ba0a303417
Matthew John <matthew@dockstudios.co.uk>
2023-03-06 17:04:10 +0000

    Initial attempt at moving to chrome.webNavigation.onCommitted to get URL navigation reason

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
 // Add a listener for URL changes
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-    // Send a message to the content script with the new URL
-    chrome.tabs.sendMessage(tabId, {type: 'urlChange', url: tab.url});
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//   console.log(tabId);
+//   if (changeInfo.status === 'complete') {
+//     // Send a message to the content script with the new URL
+    
+//   }
+// });
+
+
+chrome.webNavigation.onCommitted.addListener(function(details) {
+  // Only check for requests of the actual tab
+  if (details.frameId === 0) {
+    if (details.transitionType === 'typed') {
+      chrome.tabs.sendMessage(details.tabId, {type: 'goto', url: details.url});
+    } else {
+      chrome.tabs.sendMessage(details.tabId, {type: 'urlChange', url: details.url});
+    }
   }
 });
--- content.js
+++ content.js
@@ -1,32 +1,37 @@
 
-function addStep(step) {
-  chrome.storage.local.get('jmonData', function(previousSteps) {
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-      return;
-    }
-    allSteps = previousSteps.jmonData || [];
-    allSteps.push(step);
+// function addStep(step) {
+//   chrome.storage.local.get('jmonData', function(previousSteps) {
+//     if (chrome.runtime.lastError) {
+//       console.error(chrome.runtime.lastError);
+//       return;
+//     }
+//     allSteps = previousSteps.jmonData || [];
+//     allSteps.push(step);
 
-    console.log("All steps:");
-    console.log(allSteps);
+//     console.log("All steps:");
+//     console.log(allSteps);
 
-    chrome.storage.local.set({ jmonData: allSteps }, function() {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
-        return;
-      }
-    });
-  });
-}
+//     chrome.storage.local.set({ jmonData: allSteps }, function() {
+//       if (chrome.runtime.lastError) {
+//         console.error(chrome.runtime.lastError);
+//         return;
+//       }
+//     });
+//   });
+// }
 
 
 
 // Listen for URL change messages from the background script
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  console.log("Got message")
+  console.log(message);
+  if (message.type === 'goto') {
+    addStep({goto: message.url})
+  }
   if (message.type === 'urlChange') {
     // Log the new URL to the console
     console.log('New URL:', message.url);
-    addStep({goto: message.url});
+    addStep({check: {url: message.url}});
   }
 });
